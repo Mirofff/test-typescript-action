404f95fac617b665753086be698712d0dda3ba1a
Miroslav Zherenkov <miroslavzherenkov@outlook.com>
2023-06-13 04:23:24 +0200

    21 try to make valid github action.yml

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import * as core from "@actions/core"
+import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-try {
-    const name = core.getInput('who-to-greet')
-    console.log(`Hello ${name}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error: any) {
-    core.setFailed(error.message)
+async function main() {
+    try {
+        const name = core.getInput("Name");
+        console.log(`Hello ${name}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        const payload = JSON.stringify(github.context.payload, undefined, 2);
+        console.log(`The event payload: ${payload}`);
+    } catch (error: any) {
+        core.setFailed(error.message)
+    }
 }
